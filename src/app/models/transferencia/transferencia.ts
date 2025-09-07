198e14dda4e7f9398d534d044996a1ba89e2978a
Diego Marques <fariia_di@hotmail.com>
2025-09-07 14:16:26 -0300

    transferencia service

--- src/app/models/transferencia/transferencia.ts
+++ src/app/models/transferencia/transferencia.ts
@@ -1,2 +1,16 @@
 export interface Transferencia {
+  id?: any;
+  contaOrigem: string;
+  contaDestino: string;
+  valor: number;
+  taxa: number;
+  dataTransferencia: string;
+  dataAgendamento: string;  
+}
+
+export interface TransferenciaRequest {
+  contaOrigem: string;
+  contaDestino: string;
+  valor: number;
+  dataTransferencia: string;
 }
--- src/app/services/transferencia.services.ts
+++ src/app/services/transferencia.services.ts
@@ -1,8 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Transferencia, TransferenciaRequest } from '../models/transferencia/transferencia';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaServices {
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createAgendamento(transferencia: TransferenciaRequest): Observable<Transferencia> {
+    return this.http.post<Transferencia>(`${API_CONFIG.baseUrl}/createAgendamento`, transferencia);
+  }
+
+  findExtrato(): Observable<Transferencia[]> {
+    return this.http.get<Transferencia[]>(`${API_CONFIG.baseUrl}/agendamentos`);
+  }
   
 }
